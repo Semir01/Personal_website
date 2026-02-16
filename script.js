53ee0e1d9c1f83d371870b3715fb0cf462db4285
semir <semirjazvin3@gmail.com>
2026-02-16 20:56:25 +0100

    Setting details for project

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch("menu.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("menu").innerHTML = data;
+})
+.catch(error => console.error("Error loading menu:", error));   
