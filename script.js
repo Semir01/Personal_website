c5fe8c3862a90f5e3170cee261c27a4a98f0d34f
semir <semirjazvin3@gmail.com>
2026-02-17 20:52:07 +0100

    Added functionaliti for opening and closing menu

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-fetch("menu.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("menu").innerHTML = data;
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("menu.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("menu").innerHTML = data;
+            const menuIcon = document.querySelector(".menu-icon");
+            const menuContainer = document.querySelector(".menu-container");
+            const overlayer = document.querySelector(".overlayer");
+
+            menuIcon.addEventListener("click", () => {
+                menuContainer.classList.toggle("active");
+                overlayer.classList.toggle("active");
+            });
+
+            overlayer .addEventListener("click", () => {
+                menuContainer.classList.remove("active");
+                overlayer.classList.remove("active");
+            });
+        })
+        .catch(error => console.error("Error loading menu:", error));
 })
-.catch(error => console.error("Error loading menu:", error));   
